3deca5f4933a850bec45d829aeca2985a3cb8915
yanshanshan <598811648@qq.com>
2016-07-23 10:21:59 +0800

    2048 init

---
+++ 2048.js
@@ -0,0 +1,72 @@
+/**
+ * Created by db on 16/7/23.
+ */
+
+/***
+ * 返回随机整数，该随机整数
+ * @param min  整数min
+ * @param max  整数max
+ * @returns {*}   >=min <= max
+ */
+function getRandomInt(min, max){
+    return min + Math.floor(Math.random() * (max - min + 1))
+}
+
+/****
+ * 返回整数2或者4，因为我们对程序只需要随机生成2或者4
+ * @returns {number} 整数2或者4
+ */
+function getRandomNum(){
+    var arr = [2, 4]
+    var index = getRandomInt(0, 1)
+
+    return arr[index]
+}
+
+
+/***
+ * 返回页面内节点的innerText值为空的所有节点
+ * @returns {Array} 节点数组
+ */
+function getAllEmptyNodes(){
+    var emptyNodes = []
+    var nodes = document.getElementsByTagName('td')
+    for(var i = 0; i < nodes.length; i++){
+        if(nodes[i].innerText == ''){
+            emptyNodes.push(nodes[i])
+        }
+    }
+    return emptyNodes
+}
+
+/****
+ * 返回一个随机的内容为空的节点
+ * @returns {*}
+ */
+function getRandomEmptyNode(){
+    var emptyNodes = getAllEmptyNodes()
+    if(emptyNodes.length == 0){
+        return null
+    }
+    var randomIndex = getRandomInt(0, emptyNodes.length - 1)
+    return emptyNodes[randomIndex]
+}
+
+//2048程序初始化
+function init(){
+    var num1 = getRandomNum()
+    var num2 = getRandomNum()
+
+    var node1 = getRandomEmptyNode()
+    node1.innerText = num1
+
+    var node2 = getRandomEmptyNode()
+    node2.innerText = num2
+}
+
+//程序的入口函数
+function main(){
+    init()
+}
+
+main()
