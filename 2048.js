20ccee5ff1189749f4ab2cdc855052a8c5767f0a
yanshanshan <598811648@qq.com>
2016-07-26 21:45:32 +0800

    移动功能初步实现，代码稍作整理

--- 2048.js
+++ 2048.js
@@ -23,7 +23,6 @@ function getRandomNum() {
     return arr[index]
 }
 
-
 /***
  * 返回页面内节点的innerText值为空的所有节点
  * @returns {Array} 节点数组
@@ -67,21 +66,29 @@ function init() {
 }
 //获取所有节点（有数字内容的与空的）空的置为0，都放到一个arr的数组内
 function getNumbersFromNodes(nodes) {
-    var arr = [], node, num
+    var arr = [], node
     for (var i = 0; i < nodes.length; i++) {
         node = nodes[i]
-        //num = node.innerText == '' ? 0 : parseInt(node.innerText)
-        //if(node.innerText == ''){
-        //    num = 0
-        //}else{
-        //    num = parseInt(node.innerText)
-        //}
+
         arr.push(node.innerText == '' ? 0 : parseInt(node.innerText))
     }
-
-    return arr
+     return arr
 }
+//数值改变背景色改变操作
+function backgroundStyle(){
+    var nodesValue = getNumbersFromNodes()
+    for (var i = 0; i < nodesValue.length; i++){
+        if(nodesValue[i] === 2 || 4){
+
+        }
+
+    }
+}
+
+
+
 //获取所有节点的值并将其分为4个数组放进一个大数组，组成一个多维数组
+//—————————————适应左右移的数组——————————————
 function getGroup(numbers) {
     var group = []
     for (var i = 4; i <= numbers.length; i += 4) {
@@ -91,11 +98,10 @@ function getGroup(numbers) {
         }
         group.push(single)
     }
-    console.log('group1:'+ group)
-    return group
+      return group
 }
 
-//获取上移的4个数组（开始）
+//—————————————适应上下移的数组——————————————
 function getGroup_ud(numbers) {
     var group = []
     for (i = 0; i < 4; i++) {
@@ -105,283 +111,46 @@ function getGroup_ud(numbers) {
         }
         group.push(single)
     }
-    console.log('group:----' + group)
-    return group
-
+     return group
 }
 
+//获取结点值转整数 且 执行相加操作  并把相加后的值设回节点
+//———————————————适应上移和左移操作————————————————
 
-function getTotalMoveFlag_u(numbers) {
-    var flag = false
-
-
-    var group = getGroup_ud(numbers)
-    console.log('group2' + group)
-
-    //对每一组数据，进行判断：是否具备左移条件，如果任意一组数具备左移动条件，则代表整体具有左移条件
-    for (var i = 0; i < group.length; i++) {
-        var singleFlag = getSingleMoveFlag(group[i])
-        if (singleFlag == true) {
-            flag = true
-            break
+function resetRow(row) {
+    //获取节点值并转整数
+    var row_list = [], node, nodeValue, number
+    for (var i = 0; i < row.length; i++) {
+        node = row[i]
+        nodeValue = node.innerText
+        if (nodeValue === '') {
+            continue
+        } else {
+            number = parseInt(nodeValue)
+            row_list.push(number)
         }
     }
-
-    return flag
-}
-
-
-//获取上移的4个数组（结束）
-
-
-
-
-//下移功能添加代码（开始）
-function getTotalMoveFlag_d(numbers) {
-    var flag = false
-
-    //根据numbers，对16个数字分成4组
-    var group = getGroup_ud(numbers)
-
-    //对每一组数据，进行判断：是否具备右移条件，如果任意一组数具备右移动条件，则代表整体具有右移条件
-    for (var i = 0; i < group.length; i++) {
-        var singleFlag = getSingleMoveFlag_r(group[i])
-        if (singleFlag == true) {
-            flag = true
-            break
+    //执行加法操作
+    var row_list2 = [], curr, next, sum
+    for (var j = 0; j < row_list.length; j++) {
+        curr = row_list[j]
+        next = row_list[j + 1]
+        if (curr === next) {
+            sum = (curr + next)
+            row_list2.push(sum)
+            j++
+        } else {
+            row_list2.push(row_list[j])
         }
     }
-
-    return flag
-}
-
-
-
-
-//下移功能添加代码（结束）
-
-
-
-function getTotalMoveFlag(numbers) {
-    var flag = false
-
-    //根据numbers，对16个数字分成4组
-    var group = getGroup(numbers)
-
-    //对每一组数据，进行判断：是否具备左移条件，如果任意一组数具备左移动条件，则代表整体具有左移条件
-    for (var i = 0; i < group.length; i++) {
-        var singleFlag = getSingleMoveFlag(group[i])
-        if (singleFlag == true) {
-            flag = true
-            break
-        }
+    //把结果设置回节点上
+    for (var k = 0; k < row.length; k++) {
+        row[k].innerText = row_list2[k] || ''
     }
-
-    return flag
 }
-
-function getSingleMoveFlag(numbers) {
-    var flag = false
-    var isEmpty = false
-    for (var k = 0; k < numbers.length; k++) {
-
-        //如果有相邻的2个数相等，则说明具备左移动条件
-        if ((k + 1) < numbers.length && numbers[k] == numbers[k + 1] && numbers[k] !== 0) {
-            flag = true
-            break
-        }
-
-        //如果前面有一个空位，并且空位后面任意位置上有一个非空数字，则具备左移动条件
-        if (isEmpty && numbers[k] !== 0) {
-            flag = true
-            break
-        }
-        //记录是否找到空位
-        if (numbers[k] == 0) {
-            isEmpty = true
-        }
-    }
-
-    return flag
-}
-//自己
-function getSingleMoveFlag_r(numbers) {
-    var flag = false
-    var isEmpty = false
-    for (var k = numbers.length - 1; k > -1; k--) {
-        if (numbers[k] == numbers[k - 1] && numbers[k] != 0) {
-            flag = true
-            break
-        }
-
-        if (isEmpty && numbers[k] !== 0) {
-            flag = true
-            break
-        }
-        if (numbers[k] == 0) {
-            isEmpty = true
-        }
-
-    }
-    return flag
-}
-
-
-function getTotalMoveFlag_r(numbers) {
-    var flag = false
-
-    //根据numbers，对16个数字分成4组
-    var group = getGroup(numbers)
-
-    //对每一组数据，进行判断：是否具备右移条件，如果任意一组数具备右移动条件，则代表整体具有右移条件
-    for (var i = 0; i < group.length; i++) {
-        var singleFlag = getSingleMoveFlag_r(group[i])
-        if (singleFlag == true) {
-            flag = true
-            break
-        }
-    }
-
-    return flag
-}
-
-
-//自己
-function moveLeft() {
-    console.log('moveLeft')
-
-    //获取所有节点
-    var nodes = getAllNodes()
-    //获取所有节点对应的数字，空字符串用0代替
-    var numbers = getNumbersFromNodes(nodes)
-    //根据数字，判断是否符合左移条件
-    var flag = getTotalMoveFlag(numbers)
-
-    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
-    if (flag == false) {
-        return
-    }
-
-    //执行合并操作
-    var row1 = nodes.slice(0, 4)
-    var row2 = nodes.slice(4, 8)
-    var row3 = nodes.slice(8, 12)
-    var row4 = nodes.slice(12, 16)
-    resetRow(row1)
-    resetRow(row2)
-    resetRow(row3)
-    resetRow(row4)
-
-
-    //执行生成新数字操作
-    var randomEmptyNode = getRandomEmptyNode()
-    var randomNum = getRandomNum()
-    randomEmptyNode.innerText = randomNum
-}
-
-
-function moveUp() {
-    console.log('moveUp')
-    //获取所有节点
-    var nodes = getAllNodes()
-    //获取所有节点对应的数字，空字符串用0代替
-    var numbers = getNumbersFromNodes(nodes)
-    //根据数字，判断是否符合左移条件
-    var flag = getTotalMoveFlag_u(numbers)
-    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
-    if (flag == false) {
-        return
-    }
-
-    //执行合并操作
-    var row1 = document.getElementsByClassName('t1')
-    var row2 = document.getElementsByClassName('t2')
-    var row3 = document.getElementsByClassName('t3')
-    var row4 = document.getElementsByClassName('t4')
-    resetRow(row1)
-    resetRow(row2)
-    resetRow(row3)
-    resetRow(row4)
-
-
-    var randomEmptyNode = getRandomEmptyNode()
-    var randomNum = getRandomNum()
-    randomEmptyNode.innerText = randomNum
-
-}
-
-
-function moveRight() {
-    console.log('moveRight')
-    //获取所有节点
-    var nodes = getAllNodes()
-    //获取所有节点对应的数字，空字符串用0代替
-    var numbers = getNumbersFromNodes(nodes)
-
-    var flag = getTotalMoveFlag_r(numbers)
-
-    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
-    if (flag == false) {
-        return
-    }
-
-
-    var row1r = nodes.slice(0, 4)
-    var row2r = nodes.slice(4, 8)
-    var row3r = nodes.slice(8, 12)
-    var row4r = nodes.slice(12, 16)
-    resetRow_2(row1r)
-    resetRow_2(row2r)
-    resetRow_2(row3r)
-    resetRow_2(row4r)
-
-
-    var randomEmptyNode = getRandomEmptyNode()
-    var randomNum = getRandomNum()
-    randomEmptyNode.innerText = randomNum
-}
-
-
-function moveDown() {
-    console.log('moveDown')
-    //获取所有节点
-    var nodes = getAllNodes()
-    //获取所有节点对应的数字，空字符串用0代替
-    var numbers = getNumbersFromNodes(nodes)
-
-    var flag = getTotalMoveFlag_d(numbers)
-
-    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
-    if (flag == false) {
-        return
-    }
-
-
-
-
-
-
-    //执行合并操作
-    var row1 = document.getElementsByClassName('t1')
-    var row2 = document.getElementsByClassName('t2')
-    var row3 = document.getElementsByClassName('t3')
-    var row4 = document.getElementsByClassName('t4')
-    resetRow_2(row1)
-    resetRow_2(row2)
-    resetRow_2(row3)
-    resetRow_2(row4)
-
-
-    var randomEmptyNode = getRandomEmptyNode()
-    var randomNum = getRandomNum()
-    randomEmptyNode.innerText = randomNum
-
-}
-
-
+//———————————————适应下移和右移操作————————————————
 function resetRow_2(row) {
-    //获取节点值并转整数(右)
-    var row_list_r = [], node, nodeValue, number
+     var row_list_r = [], node, nodeValue, number
     for (var i = row.length - 1; i > -1; i--) {
         node = row[i]
         nodeValue = node.innerText
@@ -404,56 +173,235 @@ function resetRow_2(row) {
         } else {
             row_list_r2.push(curr_r)
         }
-
     }
     console.log('row_list_r2:' + row_list_r2)
 
     for (var j = 0; j < 4; j++) {
         row[3 - j].innerText = row_list_r2[j] || ''
     }
+}
 
+//对第一组数据进行是否还可以移动判断
+//——————————————适应上下移判断——————————————
+function getTotalMoveFlag_u(numbers) {
+    var flag = false
+    var group = getGroup_ud(numbers)
+    console.log('group2' + group)
+    //对每一组数据，进行判断：是否具备上移条件，如果任意一组数具备上移动条件，则代表整体具有上移条件
+    for (var i = 0; i < group.length; i++) {
+        var singleFlag = getSingleMoveFlag(group[i])
+        if (singleFlag == true) {
+            flag = true
+            break
+        }
+    }
+    return flag
+}
+//——————————————适应下移判断——————————————
+function getTotalMoveFlag_d(numbers) {
+    var flag = false
+    //根据numbers，对16个数字分成4组
+    var group = getGroup_ud(numbers)
+    //对每一组数据，进行判断：是否具备右移条件，如果任意一组数具备右移动条件，则代表整体具有右移条件
+    for (var i = 0; i < group.length; i++) {
+        var singleFlag = getSingleMoveFlag_r(group[i])
+        if (singleFlag == true) {
+            flag = true
+            break
+        }
+    }
+
+    return flag
+}
+//——————————————适应左移判断——————————————
+function getTotalMoveFlag(numbers) {
+    var flag = false
+    //根据numbers，对16个数字分成4组
+    var group = getGroup(numbers)
+    //对每一组数据，进行判断：是否具备左移条件，如果任意一组数具备左移动条件，则代表整体具有左移条件
+    for (var i = 0; i < group.length; i++) {
+        var singleFlag = getSingleMoveFlag(group[i])
+        if (singleFlag == true) {
+            flag = true
+            break
+        }
+    }
+    return flag
+}
+//——————————————适应右移判断——————————————
+function getTotalMoveFlag_r(numbers) {
+    var flag = false
+    //根据numbers，对16个数字分成4组
+    var group = getGroup(numbers)
+    //对每一组数据，进行判断：是否具备右移条件，如果任意一组数具备右移动条件，则代表整体具有右移条件
+    for (var i = 0; i < group.length; i++) {
+        var singleFlag = getSingleMoveFlag_r(group[i])
+        if (singleFlag == true) {
+            flag = true
+            break
+        }
+    }
+
+    return flag
+}
+
+//对每一行进行移动判断
+//————————————适应上左移判断———————————————
+function getSingleMoveFlag(numbers) {
+    var flag = false
+    var isEmpty = false
+    for (var k = 0; k < numbers.length; k++) {
+        //如果有相邻的2个数相等，则说明具备左移动条件
+        if ((k + 1) < numbers.length && numbers[k] == numbers[k + 1] && numbers[k] !== 0) {
+            flag = true
+            break
+        }
+        //如果前面有一个空位，并且空位后面任意位置上有一个非空数字，则具备左移动条件
+        if (isEmpty && numbers[k] !== 0) {
+            flag = true
+            break
+        }
+        //记录是否找到空位
+        if (numbers[k] == 0) {
+            isEmpty = true
+        }
+    }
+    return flag
+}
+//————————————适应下右移判断———————————————
+function getSingleMoveFlag_r(numbers) {
+    var flag = false
+    var isEmpty = false
+    for (var k = numbers.length - 1; k > -1; k--) {
+        if (numbers[k] == numbers[k - 1] && numbers[k] != 0) {
+            flag = true
+            break
+        }
+
+        if (isEmpty && numbers[k] !== 0) {
+            flag = true
+            break
+        }
+        if (numbers[k] == 0) {
+            isEmpty = true
+        }
+
+    }
+    return flag
+}
+
+//移动功能入口
+//—————————左————————————
+function moveLeft() {
+    console.log('moveLeft')
+    //获取所有节点
+    var nodes = getAllNodes()
+    //获取所有节点对应的数字，空字符串用0代替
+    var numbers = getNumbersFromNodes(nodes)
+    //根据数字，判断是否符合左移条件
+    var flag = getTotalMoveFlag(numbers)
+    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
+    if (flag == false) {
+        return
+    }
+    //执行合并操作
+    var row1 = nodes.slice(0, 4)
+    var row2 = nodes.slice(4, 8)
+    var row3 = nodes.slice(8, 12)
+    var row4 = nodes.slice(12, 16)
+    resetRow(row1)
+    resetRow(row2)
+    resetRow(row3)
+    resetRow(row4)
+    //执行生成新数字操作
+    var randomEmptyNode = getRandomEmptyNode()
+    var randomNum = getRandomNum()
+    randomEmptyNode.innerText = randomNum
+}
+//————————————上—————————————
+function moveUp() {
+    console.log('moveUp')
+    //获取所有节点
+    var nodes = getAllNodes()
+    //获取所有节点对应的数字，空字符串用0代替
+    var numbers = getNumbersFromNodes(nodes)
+    //根据数字，判断是否符合上移条件
+    var flag = getTotalMoveFlag_u(numbers)
+    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
+    if (flag == false) {
+        return
+    }
+    //执行合并操作
+    var row1 = document.getElementsByClassName('t1')
+    var row2 = document.getElementsByClassName('t2')
+    var row3 = document.getElementsByClassName('t3')
+    var row4 = document.getElementsByClassName('t4')
+    resetRow(row1)
+    resetRow(row2)
+    resetRow(row3)
+    resetRow(row4)
+    //执行生成新数字操作
+    var randomEmptyNode = getRandomEmptyNode()
+    var randomNum = getRandomNum()
+    randomEmptyNode.innerText = randomNum
 
 }
 
-
-
-
-function resetRow(row) {
-    //获取节点值并转整数(左)
-    var row_list = [], node, nodeValue, number
-    for (var i = 0; i < row.length; i++) {
-        node = row[i]
-        nodeValue = node.innerText
-        if (nodeValue === '') {
-            continue
-        } else {
-            number = parseInt(nodeValue)
-            row_list.push(number)
-        }
-
-
+//————————————右———————————————————
+function moveRight() {
+    console.log('moveRight')
+    //获取所有节点
+    var nodes = getAllNodes()
+    //获取所有节点对应的数字，空字符串用0代替
+    var numbers = getNumbersFromNodes(nodes)
+    //根据数字，判断是否符合右移条件
+    var flag = getTotalMoveFlag_r(numbers)
+    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
+    if (flag == false) {
+        return
     }
+    //执行合并操作
+    var row1r = nodes.slice(0, 4)
+    var row2r = nodes.slice(4, 8)
+    var row3r = nodes.slice(8, 12)
+    var row4r = nodes.slice(12, 16)
+    resetRow_2(row1r)
+    resetRow_2(row2r)
+    resetRow_2(row3r)
+    resetRow_2(row4r)
+    //执行生成新数字操作
+    var randomEmptyNode = getRandomEmptyNode()
+    var randomNum = getRandomNum()
+    randomEmptyNode.innerText = randomNum
+}
 
-
-    //执行加法操作
-    var row_list2 = [], curr, next, sum
-    for (var j = 0; j < row_list.length; j++) {
-        curr = row_list[j]
-        next = row_list[j + 1]
-        if (curr === next) {
-            sum = (curr + next)
-            row_list2.push(sum)
-            j++
-        } else {
-            row_list2.push(row_list[j])
-        }
+//————————————————下———————————————————
+function moveDown() {
+    console.log('moveDown')
+    //获取所有节点
+    var nodes = getAllNodes()
+    //获取所有节点对应的数字，空字符串用0代替
+    var numbers = getNumbersFromNodes(nodes)
+    //根据数字，判断是否符合下移条件
+    var flag = getTotalMoveFlag_d(numbers)
+    //如果不符合移动条件，就直接返回，不做任何合并和生成新数字的操作
+    if (flag == false) {
+        return
     }
+    //执行合并操作
+    var row1 = document.getElementsByClassName('t1')
+    var row2 = document.getElementsByClassName('t2')
+    var row3 = document.getElementsByClassName('t3')
+    var row4 = document.getElementsByClassName('t4')
+    resetRow_2(row1)
+    resetRow_2(row2)
+    resetRow_2(row3)
+    resetRow_2(row4)
+    //执行生成新数字操作
+    var randomEmptyNode = getRandomEmptyNode()
+    var randomNum = getRandomNum()
+    randomEmptyNode.innerText = randomNum
 
-    //把结果设置回节点上
-
-    for (var k = 0; k < row.length; k++) {
-        row[k].innerText = row_list2[k] || ''
-    }
 }
 
 //程序的入口函数
@@ -480,3 +428,4 @@ function main() {
 }
 
 main()
+
